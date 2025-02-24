da15da4a97b8c000146c56e5a43cf3b1282391b4
madstone290 <madstone290@gmail.com>
2025-02-24 22:59:38 +0900

    Union Find

---
+++ ConsoleApp/DataStructures/UnionFind.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.DataStructures
+{
+    public class UnionFind
+    {
+        private int _size;
+
+        private int[] _unionSizes;
+
+        private int[] _parents;
+
+        private int _unionCount;
+
+        public UnionFind(int size)
+        {
+            _size = size;
+            _unionSizes = new int[size];
+            _parents = new int[size];
+            _unionCount = size;
+            
+            for (int i = 0; i < size; i++)
+            {
+                _unionSizes[i] = 1;
+                _parents[i] = i;
+            }
+        }
+
+        /// <summary>
+        /// Find which uninon 'i' belongs to
+        /// </summary>
+        /// <param name="i"></param>
+        /// <returns></returns>
+        public int Find(int i)
+        {
+            // if parent equals to child, then it the parent(child) is the root.
+            int root = i;
+            while(root != _parents[root])
+            {
+                root = _parents[root];
+            }
+
+            // Use path compression
+            int current = i;
+            while(current != root)
+            {
+                int next = _parents[current];
+                _parents[current] = root;
+                current = next;
+            }
+
+            return root;
+        }
+
+        public void Unify(int i1, int i2)
+        {
+            int root1 = Find(i1);
+            int root2 = Find(i2);
+
+            if (root1 == root2)
+                return;
+
+            if (root1 < root2)
+            {
+                _unionSizes[root1] += _unionSizes[root2];
+                _parents[root2] = root1;
+            }
+            else
+            {
+                _parents[root2] += _unionSizes[root1];
+                _parents[root1] = root2;
+            }
+            _unionCount--;
+        }
+
+        public bool Connected(int i1, int i2)
+        {
+            return Find(i1) == Find(i2);
+        }
+
+        public int UnionSize(int i)
+        {
+            return _unionSizes[Find(i)];
+        }
+
+        public int Size()
+        {
+            return _size;
+        }
+
+        public int UnionCount()
+        {
+            return _unionCount;
+        }
+
+
+    }
+}
---
+++ TestProject/DataStructures/Test1.cs
@@ -0,0 +1,108 @@
+﻿using ConsoleApp.DataStructures;
+
+namespace TestProject.DataStructures
+{
+    [TestClass]
+    public class UnionFindTests
+    {
+        [TestMethod]
+        public void TestUnionFind_Initialization()
+        {
+            int size = 10;
+            UnionFind uf = new UnionFind(size);
+
+            Assert.AreEqual(size, uf.Size());
+            Assert.AreEqual(size, uf.UnionCount());
+
+            for (int i = 0; i < size; i++)
+            {
+                Assert.AreEqual(i, uf.Find(i));
+                Assert.AreEqual(1, uf.UnionSize(i));
+            }
+        }
+
+        [TestMethod]
+        public void TestUnionFind_Unify()
+        {
+            UnionFind uf = new UnionFind(10);
+
+            uf.Unify(1, 2);
+            Assert.IsTrue(uf.Connected(1, 2));
+            Assert.AreEqual(9, uf.UnionCount());
+
+            uf.Unify(2, 3);
+            Assert.IsTrue(uf.Connected(1, 3));
+            Assert.AreEqual(8, uf.UnionCount());
+
+            uf.Unify(4, 5);
+            Assert.IsTrue(uf.Connected(4, 5));
+            Assert.AreEqual(7, uf.UnionCount());
+
+            uf.Unify(1, 5);
+            Assert.IsTrue(uf.Connected(1, 4));
+            Assert.AreEqual(6, uf.UnionCount());
+        }
+
+        [TestMethod]
+        public void TestUnionFind_Connected()
+        {
+            UnionFind uf = new UnionFind(10);
+
+            uf.Unify(1, 2);
+            uf.Unify(2, 3);
+            uf.Unify(4, 5);
+
+            Assert.IsTrue(uf.Connected(1, 3));
+            Assert.IsFalse(uf.Connected(1, 4));
+            Assert.IsTrue(uf.Connected(4, 5));
+        }
+
+        [TestMethod]
+        public void TestUnionFind_UnionSize()
+        {
+            UnionFind uf = new UnionFind(10);
+
+            uf.Unify(1, 2);
+            uf.Unify(2, 3);
+
+            Assert.AreEqual(3, uf.UnionSize(uf.Find(1)));
+            Assert.AreEqual(3, uf.UnionSize(uf.Find(2)));
+            Assert.AreEqual(3, uf.UnionSize(uf.Find(3)));
+
+            uf.Unify(4, 5);
+            Assert.AreEqual(2, uf.UnionSize(uf.Find(4)));
+            Assert.AreEqual(2, uf.UnionSize(uf.Find(5)));
+        }
+
+        [TestMethod]
+        public void TestUnionFind_Size()
+        {
+            UnionFind uf = new UnionFind(10);
+            uf.Unify(0, 1);
+            uf.Unify(1, 2);
+            uf.Unify(2, 3);
+            uf.Unify(3, 4);
+            uf.Unify(4, 5);
+            uf.Unify(5, 6);
+            uf.Unify(6, 7);
+            uf.Unify(7, 8);
+            uf.Unify(8, 9);
+
+            Assert.AreEqual(10, uf.Size());
+            Assert.AreEqual(1, uf.UnionCount());
+            Assert.AreEqual(true, uf.Connected(0, 9));
+            Assert.AreEqual(10, uf.UnionSize(0));
+            Assert.AreEqual(10, uf.UnionSize(1));
+            Assert.AreEqual(10, uf.UnionSize(2));
+            Assert.AreEqual(10, uf.UnionSize(3));
+            Assert.AreEqual(10, uf.UnionSize(4));
+            Assert.AreEqual(10, uf.UnionSize(5));
+            Assert.AreEqual(10, uf.UnionSize(6));
+            Assert.AreEqual(10, uf.UnionSize(7));
+            Assert.AreEqual(10, uf.UnionSize(8));
+            Assert.AreEqual(10, uf.UnionSize(9));
+
+
+        }
+    }
+}
